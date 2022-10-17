2d685a968f6d5123ec2a3f32efc06d8f7fe95aa5
Tobias Baier <tobias.baiier@web.de>
2022-10-17 22:28:33 +0200

    homepage change

--- src/components/Gigs.js
+++ src/components/Gigs.js
@@ -7,15 +7,6 @@ function Gigs() {
     return(
         <div className="Card">
       <Card className="Card1" border="dark" style={{ width: '18rem' }}>
-        <Card.Header> Sonntag, 16.10.2022, 08:45 Uhr </Card.Header>
-        <Card.Body>
-          <Card.Title>Kirchweih Schalding</Card.Title>
-          <Card.Text>
-            Rhythmischer Gottesdienst mit anschließendem Frühshoppen im Pfarrsaal
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className="Card1" border="dark" style={{ width: '18rem' }}>
         <Card.Header>Samstag, 22.10.2022, 14:00 Uhr</Card.Header>
         <Card.Body>
           <Card.Title>SV Schalding - TSV Dachau</Card.Title>
