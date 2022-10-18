b220976109c879311e8127f879d0b5c182e7830f
Tobias Baier <tobias.baiier@web.de>
2022-10-18 21:21:12 +0200

    Updates

---
+++ precache-manifest.27e1660303a086788fffd94b7e13084e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "4654192e3bfd94e8a8d2",
+    "url": "/baierbuamandfriends/static/css/main.8c6219b0.chunk.css"
+  },
+  {
+    "revision": "4654192e3bfd94e8a8d2",
+    "url": "/baierbuamandfriends/static/js/main.4654192e.chunk.js"
+  },
+  {
+    "revision": "cd474f2dcda6b2605ecf",
+    "url": "/baierbuamandfriends/static/js/1.cd474f2d.chunk.js"
+  },
+  {
+    "revision": "ed034c325c79f1d0a42e",
+    "url": "/baierbuamandfriends/static/css/2.ed17cf27.chunk.css"
+  },
+  {
+    "revision": "ed034c325c79f1d0a42e",
+    "url": "/baierbuamandfriends/static/js/2.ed034c32.chunk.js"
+  },
+  {
+    "revision": "53e692d7c6aa99f134e1",
+    "url": "/baierbuamandfriends/static/js/runtime~main.53e692d7.js"
+  },
+  {
+    "revision": "4f7cb5fbb28d0f9ddc97641f8b288735",
+    "url": "/baierbuamandfriends/static/media/instagram.4f7cb5fb.png"
+  },
+  {
+    "revision": "ea59ced004c4f8a47f732af9ea4d722d",
+    "url": "/baierbuamandfriends/static/media/logo1.ea59ced0.png"
+  },
+  {
+    "revision": "5c5e3b4fa0e9cf66d213e187e091f5c0",
+    "url": "/baierbuamandfriends/static/media/image00001.5c5e3b4f.jpeg"
+  },
+  {
+    "revision": "1bdbe579e40919781564a27632af6a3c",
+    "url": "/baierbuamandfriends/static/media/image00002.1bdbe579.jpeg"
+  },
+  {
+    "revision": "8e01465095b5bd617fee8dfb6d0ee9d0",
+    "url": "/baierbuamandfriends/static/media/image00003.8e014650.jpeg"
+  },
+  {
+    "revision": "07689d4eaaa3d530d58826b5d7f84735",
+    "url": "/baierbuamandfriends/static/media/Montserrat-Regular.07689d4e.ttf"
+  },
+  {
+    "revision": "7958dff8efb2870a93dd835808d57d44",
+    "url": "/baierbuamandfriends/index.html"
+  }
+];
