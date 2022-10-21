0b5e689edf22700f9ac674df679e30c9ab618316
Tobias Baier <tobias.baiier@web.de>
2022-10-22 00:30:07 +0200

    Updates

---
+++ precache-manifest.d76643712ff55c52980780a64afdf9a0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "270f9586762d8c65c61f",
+    "url": "/static/css/main.54df5a31.chunk.css"
+  },
+  {
+    "revision": "270f9586762d8c65c61f",
+    "url": "/static/js/main.270f9586.chunk.js"
+  },
+  {
+    "revision": "90ecf6b5710b2396cef7",
+    "url": "/static/js/1.90ecf6b5.chunk.js"
+  },
+  {
+    "revision": "f7bb3ef8924626c552f1",
+    "url": "/static/css/2.bd8a566a.chunk.css"
+  },
+  {
+    "revision": "f7bb3ef8924626c552f1",
+    "url": "/static/js/2.f7bb3ef8.chunk.js"
+  },
+  {
+    "revision": "f63f43421bdc937aa0f2",
+    "url": "/static/js/runtime~main.f63f4342.js"
+  },
+  {
+    "revision": "4f7cb5fbb28d0f9ddc97641f8b288735",
+    "url": "/static/media/instagram.4f7cb5fb.png"
+  },
+  {
+    "revision": "ea59ced004c4f8a47f732af9ea4d722d",
+    "url": "/static/media/logo1.ea59ced0.png"
+  },
+  {
+    "revision": "5c5e3b4fa0e9cf66d213e187e091f5c0",
+    "url": "/static/media/image00001.5c5e3b4f.jpeg"
+  },
+  {
+    "revision": "1bdbe579e40919781564a27632af6a3c",
+    "url": "/static/media/image00002.1bdbe579.jpeg"
+  },
+  {
+    "revision": "8e01465095b5bd617fee8dfb6d0ee9d0",
+    "url": "/static/media/image00003.8e014650.jpeg"
+  },
+  {
+    "revision": "07689d4eaaa3d530d58826b5d7f84735",
+    "url": "/static/media/Montserrat-Regular.07689d4e.ttf"
+  },
+  {
+    "revision": "35676713b4d337e6525d3eddff3f33ef",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/baierbuam/precache-manifest.1bf061a6e46f25906d37fef05dda430e.js"
+  "/precache-manifest.d76643712ff55c52980780a64afdf9a0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/baierbuam/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
